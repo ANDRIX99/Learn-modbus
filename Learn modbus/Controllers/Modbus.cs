ab08794d53a830a382bd4853f24a73df90c00daa
Andrea <andrearuzza501@gmail.com>
2026-02-16 09:33:27 +0100

    Aggiungere i file di progetto.

---
+++ Learn modbus/Controllers/Modbus.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FluentModbus;
+
+namespace Learn_modbus.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Modbus : Controller
+    {
+        [HttpGet("read-holding")]
+        public IActionResult ReadRegister(string ip, int startAddress, int count)
+        {
+            using (var client = new ModbusTcpClient())
+            {
+                try
+                {
+                    // Connect to the Modbus server
+                    client.Connect(ip, ModbusEndianness.BigEndian);
+
+                    // Read holding registers
+                    var data = client.ReadHoldingRegisters<short>(1, startAddress, count);
+
+                    return Ok(data.ToArray());
+                } catch (Exception ex)
+                {
+                    return BadRequest(ex.Message);
+                }
+            }
+        }
+    }
+}
